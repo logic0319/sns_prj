95ecaae08357d2116da818213e9e742c9f2aa5b4
yunsoo3042 <yunsoo3042@gmail.com>
2016-11-30 23:04:10 +0900

    member url 오타수정

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,6 +4,6 @@ from .views import LoginView,LogoutView,RegisterView
 urlpatterns = [
     url(r'login/$',LoginView.as_view(), name='rest_login'),
     url(r'logout/$',LogoutView.as_view(),name='rest_logout'),
-    url(r'signup$', RegisterView.as_view(), name='rest_register'),
+    url(r'signup/$', RegisterView.as_view(), name='rest_register'),
 
 ]
