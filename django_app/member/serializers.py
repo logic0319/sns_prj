81139aef71692a1026bebd4c49d7452048b1379a
yunsoo3042 <yunsoo3042@gmail.com>
2016-11-30 21:35:10 +0900

    원격 저장소 develop브랜치와 병합 후 충돌난 부분 해결

--- django_app/member/serializers.py
+++ django_app/member/serializers.py
@@ -2,7 +2,9 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers, exceptions
-from rest_auth.models import TokenModel
+from rest_framework.authtoken.models import Token
+from rest_framework import serializers
+from member.models import CustomUser
 
 UserModel = get_user_model()
 
@@ -42,12 +44,33 @@ class LoginSerializer(serializers.Serializer):
         return attrs
 
 
+class RegisterSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+    gender = serializers.BooleanField(required=False)
+    age = serializers.DateField(required=False)
+
+    def create(self, validated_data):
+        validated_data = {
+            'email': validated_data.get('email', ''),
+            'password': validated_data.get('password1', ''),
+            'gender': validated_data.get('gender'),
+            'age': validated_data.get('age'),
+        }
+        return CustomUser.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("The two password fields didn't match.")
+        return data
+
 
 class TokenSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Token model.
-    """
 
     class Meta:
-        model = TokenModel
-        fields = ('key','user')
+        model = Token
+        fields = ('key',)
