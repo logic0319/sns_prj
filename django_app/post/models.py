bd6821816bc455056f71b3277a75e8f0654c3ec0
yunsoo3042 <yunsoo3042@gmail.com>
2016-12-01 19:44:08 +0900

    post model필드 user를 author로 변경

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -5,9 +5,9 @@ from member.models import CustomUser
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    user = models.ForeignKey(CustomUser)
-    created_date = models.DateTimeField(auto_now=True)
-    modified_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(CustomUser)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
     view_count = models.IntegerField(default=0)
     like_users = models.ManyToManyField(CustomUser, related_name='like_users_set')
     bookmark_users = models.ManyToManyField(CustomUser, related_name='bookmark_users_set')
