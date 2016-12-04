25feb723fd770a5933f23175dedf88aff69a7315
YongPilMoon <m71661@gmail.com>
2016-12-04 17:54:31 +0900

    Refactor PostDetailView
    사용되지 않거나 불필요한 코드를 제거 했습니다.

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -29,24 +29,16 @@ class PostCreateView(generics.CreateAPIView):
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
 
     def update(self, request, *args, **kwargs):
         if request.user.pk == self.get_object().author.pk:
-            pk = request.user.pk
-            request.data['author'] = pk
+            request.data['author'] = request.user.pk
             return super().update(request, *args, **kwargs)
         raise APIException({"errors": "수정 권한이 없습니다."})
 
     def destroy(self, request, *args, **kwargs):
         if request.user.pk == self.get_object().author.pk:
-            pk = request.user.pk
-            request.data['author'] = pk
+            request.data['author'] = request.user.pk
             return super().destroy(request, *args, **kwargs)
         raise APIException({"errors": "삭제 권한이 없습니다."})
 
