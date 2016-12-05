8e374c002de8cc4aa2e69aeb3c87ab8abb80b202
yunsoo3042 <yunsoo3042@gmail.com>
2016-12-05 20:25:38 +0900

    restframework에서 sessionAuthentication삭제 및 migrate재수행

--- django_app/sns_prj/settings.py
+++ django_app/sns_prj/settings.py
@@ -180,10 +180,10 @@ else:
     MEDIA_URL = '/media/'
 
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
 }
