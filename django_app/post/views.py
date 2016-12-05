d7df4528edc47bb3fe4345944946ef53d3b4d6d0
yunsoo3042 <yunsoo3042@gmail.com>
2016-12-05 20:22:21 +0900

    PostLikeView작성, create와 destroy method를 오버라이드해서 POST로 접근했을경우 postLike객체를 생성하고, DELETE로 접근했을경우 PostLIke객체를 삭제하도록 함

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -42,13 +42,38 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
             pk = request.user.pk
             request.data['author'] = pk
             return super().update(request, *args, **kwargs)
-        raise APIException({"errors": "수정 권한이 없습니다."})
+        raise APIException({"errors": "수정 권한이 없습니다."},status=status.HTTP_401_UNAUTHORIZED)
 
     def destroy(self, request, *args, **kwargs):
         if request.user.pk == self.get_object().author.pk:
             pk = request.user.pk
             request.data['author'] = pk
             return super().destroy(request, *args, **kwargs)
-        raise APIException({"errors": "삭제 권한이 없습니다."})
+        raise APIException({"errors": "삭제 권한이 없습니다."},status=status.HTTP_401_UNAUTHORIZED)
 
 
+class PostLikeView(generics.CreateAPIView,
+                   generics.DestroyAPIView):
+    queryset = PostLike.objects.all()
+    serializer_class = PostLikeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        post = kwargs['pk']
+        like_user = request.user.pk
+        if PostLike.objects.filter(post=post, like_user=like_user):
+            return Response({"errors": "이미 좋아요를 누른 글입니다"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        request.data._mutable = True
+        request.data['like_user'] = request.user.pk
+        request.data['post'] = kwargs['pk']
+        return super().create(request, *args,**kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        user = request.user.pk
+        try:
+            instance = PostLike.objects.get(post=pk,like_user=user)
+        except:
+            return Response({"errors":"아직 좋아요를 누르지 않은 글입니다"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_404_NOT_FOUND)
