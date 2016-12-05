9d9a6ad55991fc59c617e31919ace22bd92849b9
yunsoo3042 <yunsoo3042@gmail.com>
2016-12-05 21:27:32 +0900

    master branch와 feature/post_like,bookmark branch를 병합후 오류 해결

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
-from .views import PostListView, PostDetailView, PostCreateView, \
-    CommentCreateView
+from .views import PostListView, PostDetailView, PostCreateView, MyPostListView,PostLikeView, PostBookMarkView,CommentCreateView
 
 urlpatterns = [
-    url(r'^list/$', PostListView.as_view(), name='post_list'),
+    url(r'^$', PostListView.as_view(), name='post_list'),
+    url(r'^my/$', MyPostListView.as_view(), name='my_post_list'),
     url(r'^add/$', PostCreateView.as_view(), name='post_create'),
     url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'),
     url(r'^(?P<pk>\d+)/comment/$', CommentCreateView.as_view(), name='post_detail'),
+    url(r'^(?P<pk>\d+)/like/$', PostLikeView.as_view(), name='post_like'),
+    url(r'^(?P<pk>\d+)/bookmark/$', PostBookMarkView.as_view(), name='post_bookmark'),
 ]
+
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.exceptions import AuthenticationFailed
-
-from post.models import Post, Comment
-from post.serializers import PostListSerializer, PostDetailSerializer, CommentSerializer, PostCreateSerializer
+from post.models import Comment
+from post.serializers import CommentSerializer
 import django_filters
+from rest_framework import status
+from rest_framework.response import Response
+from post.models import Post, PostLike, PostBookMark
+from post.serializers import PostListSerializer, PostDetailSerializer, PostLikeSerializer, PostCreateSerializer, PostBookMarkSerializer
 
 
 class PostFilter(django_filters.rest_framework.FilterSet):
+
     class Meta:
         model = Post
         fields = ['hashtags__name', ]
@@ -20,6 +24,14 @@ class PostListView(generics.ListAPIView):
     filter_class = PostFilter
 
 
+class MyPostListView(generics.ListAPIView):
+    serializer_class = PostListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+
 class PostCreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
@@ -27,6 +39,7 @@ class PostCreateView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         pk = request.user.pk
+        request.data._mutable = True
         request.data['author'] = pk
         return super().create(request, *args, **kwargs)
 
@@ -37,6 +50,7 @@ class PostCreateView(generics.CreateAPIView):
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def update(self, request, *args, **kwargs):
         if request.user.pk == self.get_object().author.pk:
@@ -51,6 +65,60 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
         raise AuthenticationFailed(detail="삭제 권한이 없습니다.")
 
 
+class PostLikeView(generics.CreateAPIView,
+                   generics.DestroyAPIView):
+    queryset = PostLike.objects.all()
+    serializer_class = PostLikeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        post = kwargs['pk']
+        like_user = request.user.pk
+        if PostLike.objects.filter(post=post, like_user=like_user):
+            return Response({"errors": "이미 좋아요를 누른 글입니다"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        request.data._mutable = True
+        request.data['like_user'] = request.user.pk
+        request.data['post'] = kwargs['pk']
+        return super().create(request, *args,**kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        user = request.user.pk
+        try:
+            instance = PostLike.objects.get(post=pk,like_user=user)
+        except:
+            return Response({"errors":"아직 좋아요를 누르지 않은 글입니다"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class PostBookMarkView(generics.CreateAPIView,
+                       generics.DestroyAPIView):
+        queryset = PostBookMark.objects.all()
+        serializer_class = PostBookMarkSerializer
+        permission_classes = (permissions.IsAuthenticated,)
+
+        def create(self,request,*args, **kwargs):
+            post = kwargs['pk']
+            user = request.user.pk
+            if PostBookMark.objects.filter(post=post, bookmark_user=user):
+                return Response({"errors": "이미 북마크한 글입니다"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            request.data._mutable = True
+            request.data['bookmark_user'] = request.user.pk
+            request.data['post'] = kwargs['pk']
+            return super().create(request, *args, **kwargs)
+
+        def destroy(self, request, *args, **kwargs):
+            pk = kwargs['pk']
+            user = request.user.pk
+            try:
+                instance = PostBookMark.objects.get(post=pk, bookmark_user=user)
+            except:
+                return Response({"errors": "아직 북마크하지 않은 글입니다"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -60,4 +128,5 @@ class CommentCreateView(generics.CreateAPIView):
         request.data['author'] = request.user.pk
         request.data['post'] = kwargs.get('pk')
         return super().create(request, *args, **kwargs)
+
 
