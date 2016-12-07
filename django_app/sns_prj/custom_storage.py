6a7f1553b1a1c616f6d7efdb450269a47072c846
YongPilMoon <m71661@gmail.com>
2016-12-07 11:31:28 +0900

    Set Timezone and Add Randomfilename class
    local time을 측정하기위해 time zone을 setting하고 img파일 저장시 같은 이름의 사진이 덮어지는 것을 방지하기위한 Randomfilename class를 추가하였습니다.

---
+++ django_app/sns_prj/custom_storage.py
@@ -0,0 +1,26 @@
+import os
+import uuid
+
+from django.conf import settings
+from django.utils.deconstruct import deconstructible
+from storages.backends.s3boto import S3BotoStorage
+
+
+class StaticStorage(S3BotoStorage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3BotoStorage):
+    location = settings.MEDIAFILES_LOCATION
+
+
+@deconstructible
+class RandomFileName(object):
+    def __init__(self, path):
+        self.path = os.path.join(path, "%s%s")
+        print(path)
+
+    def __call__(self, _, filename):
+        # @note It's up to the validators to check if it's the correct file type in name or if one even exist.
+        extension = os.path.splitext(filename)[1]
+        return self.path % (uuid.uuid4(), extension)
