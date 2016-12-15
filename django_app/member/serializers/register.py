c0d956d286b93940e8c88837a6582769754f17a7
YongPilMoon <m71661@gmail.com>
2016-12-15 13:46:45 +0900

    Add password validation

--- django_app/member/serializers/register.py
+++ django_app/member/serializers/register.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth import password_validation
+from django.core import exceptions
 from rest_framework import serializers
 from rest_framework.exceptions import APIException
 
@@ -39,4 +41,13 @@ class RegisterSerializer(serializers.Serializer):
     def validate(self, data):
         if data['password1'] != data['password2']:
             raise serializers.ValidationError("The two password fields didn't match.")
+        errors = dict()
+        try:
+            password_validation.validate_password(password=data['password1'], user=CustomUser)
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
         return data
